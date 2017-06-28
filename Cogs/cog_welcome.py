366f9f196f1a4c5898b1f903a2628a101a075abb
Yamitatsu <cg.midev@gmail.com>
2017-06-28 21:22:54 +0200

    testing

--- Cogs/cog_welcome.py
+++ Cogs/cog_welcome.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-import mysql.connector
+import pymysql
 import role_ids
 from __main__ import config
 
@@ -10,7 +10,7 @@ class Welcome():
     @bot.event
     async def on_member_join(member):
         guild = member.guild
-        conn = mysql.connector.connect(host=config['Database']['host'],user=config['Database']['user'],password=config['Database']['password'], database=config['Database']['database'])
+        connection = pymysql.connect(host=config['Database']['host'], user=config['Database']['user'], password=config['Database']['password'], db=config['Database']['database'], charset='utf8')
         cursor = conn.cursor()
         cursor.execute("""SELECT message FROM welcomes WHERE id_server = %s""", (member.guild.id))
         rows = cursor.fetchall()
