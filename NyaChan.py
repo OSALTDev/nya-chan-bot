8fb2651cf1801e07d1cea65eee0b224f77643b85
Yamitatsu <cg.midev@gmail.com>
2017-06-29 18:44:13 +0200

    testing

--- NyaChan.py
+++ NyaChan.py
@@ -124,7 +124,7 @@ async def update(ctx):
     """Update bot."""
     try:
         process = subprocess.check_output("git pull origin master", stderr=subprocess.STDOUT, shell=True)
-        print(process)
+        print(str(process))
         print("Coucou\nHey")
         print('Coucou2\nHey2')
         await ctx.send("```{}```".format(process))
