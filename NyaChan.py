e0b04a786b9cbfc3a4727d6684802b79b37a1427
Yamitatsu <cg.midev@gmail.com>
2017-06-29 18:16:00 +0200

    testing

--- NyaChan.py
+++ NyaChan.py
@@ -47,6 +47,8 @@ async def on_command_error(ctx, error):
         await ctx.send("{}, only my Master can ask me to do that, nya!```{}```".format(ctx.message.author.mention, ctx.message.content))
     elif isinstance(error, discord.ext.commands.errors.UserInputError):
         await ctx.send("{}, Input error```py\n{}: {}\n```".format(ctx.message.author.mention, type(error).__name__, str(error)))
+    elif isinstance(error, NoPrivateMessage):
+        await ctx.send("{}, this command cannot be send in a PM!```{}```".format(ctx.message.author.mention, ctx.message.content))
     else:
         await ctx.send("{}, error```py\n{}: {}\n```".format(ctx.message.author.mention, type(error).__name__, str(error)))
 
