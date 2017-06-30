119ada8967f56466b11a522ee8ea512bd7436df7
Yamitatsu <cg.midev@gmail.com>
2017-06-30 03:58:05 +0200

    testing

--- Cogs/customs.py
+++ Cogs/customs.py
@@ -9,28 +9,6 @@ class Customs():
     def __init__(self, bot):
         self.bot = bot
 
-    def get_message(self, member):
-        guild = member.guild
-        connection = pymysql.connect(host=config['Database']['host'], user=config['Database']['user'], password=config['Database']['password'], db=config['Database']['database'], charset='utf8')
-        cursor = connection.cursor()
-        cursor.execute("""SELECT message FROM welcomes WHERE id_server = %s""", (guild.id))
-        rows = cursor.fetchall()
-        if len(rows) == 0:
-            cursor.execute("""INSERT INTO welcomes (id, id_server, message) VALUES (null, %s, "")""", (guild.id))
-            connection.commit()
-            text = ""
-        else:
-            text = rows[0][0]
-        connection.close()
-        return text.format(member, guild)
-
-    async def member_join(self, member):
-        text = self.get_message(member)
-        try:
-            await member.send(text)
-        except:
-            pass
-
     @commands.command(description='Sends a custom message.')
     @commands.guild_only()
     async def cc(self, ctx, command_name : str):
