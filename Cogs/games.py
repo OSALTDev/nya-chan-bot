3f92f0e4f5b66f140219f6a61c2a0621530a8bcb
Yamitatsu <cg.midev@gmail.com>
2017-07-02 00:05:57 +0200

    testing

--- Cogs/games.py
+++ Cogs/games.py
@@ -14,13 +14,13 @@ class Games():
         roles = ctx.author.roles
         hasRole = False
         for role in roles:
-            if role == 'LFG':
+            if role.name == 'LFG':
                 hasRole = True
         if hasRole:
             ctx.author.add_roles(['LFG'])
             ctx.author.send('You are now tagged as looking for a game, {}'.format(ctx.author.mention()))
         else:
-            ctx.author.remove_Roles(['LFG'])
+            ctx.author.remove_roles(['LFG'])
             ctx.author.send('You are not tagged as looking for a game anymore, {}'.format(ctx.author.mention()))
 
 def setup(bot):
