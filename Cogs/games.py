9ba392d702d3ab7aa8a7b1b420ee97b8d4f49938
Yamitatsu <cg.midev@gmail.com>
2017-07-02 00:03:39 +0200

    testing

---
+++ Cogs/games.py
@@ -0,0 +1,28 @@
+from discord.ext import commands
+import asyncio
+import datetime
+import role_ids
+
+class Games():
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(description='Identify yourself as looking for a game (toggle command).')
+    @commands.guild_only()
+    async def lfg(self, ctx):
+        """Toggle your LFG status"""
+        roles = ctx.author.roles
+        hasRole = False
+        for role in roles:
+            if role == 'LFG':
+                hasRole = True
+        if hasRole:
+            ctx.author.add_roles(['LFG'])
+            ctx.author.send('You are now tagged as looking for a game, {}'.format(ctx.author.mention()))
+        else:
+            ctx.author.remove_Roles(['LFG'])
+            ctx.author.send('You are not tagged as looking for a game anymore, {}'.format(ctx.author.mention()))
+
+def setup(bot):
+    cog = Games(bot)
+    bot.add_cog(cog)
--- NyaChan.py
+++ NyaChan.py
@@ -10,7 +10,7 @@ import configparser
 config = configparser.ConfigParser()
 config.read_file(open('settings.ini'))
 
-startup_cogs = ['welcome', 'nsfw', 'customs', 'misc', 'rpg', 'music']
+startup_cogs = ['welcome', 'nsfw', 'customs', 'misc', 'rpg', 'games']
 
 bot = commands.Bot(command_prefix=config['Bot']['prefix'], description=config['Bot']['description'])
 
