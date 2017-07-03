199f035f45d801560e4c58e5089d3fe8039c6e8d
Yamitatsu <cg.midev@gmail.com>
2017-07-04 00:57:37 +0200

    testing

--- Cogs/squirrel.py
+++ Cogs/squirrel.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import asyncio
 import datetime
 import role_ids
+import sys, os
 
 class Squirrel():
     def __init__(self, bot):
@@ -19,16 +20,17 @@ class Squirrel():
         if squirrel_role == None:
             await ctx.channel.send('There is no Squirrel Army role on this server.')
             return False
-        future_squirrel = await commands.MemberConverter(ctx, username)
-        if future_squirrel is None:
-            await ctx.channel.send('The user {} cannot be found'.format(username))
-            return False;
-        future_squirrel.add_roles(squirrel_role)
-        squirrel_channel = self.bot.get_channel(325420488107098112)
-        if squirrel_channel is None:
-            await ctx.channel.send('The Squirrel Army channel cannot be found')
-            return False;
-        await squirrel_channel.send('Welcome in the Squirrel Army {}, happy squirreling =^.^='.format(future_squirrel.mention))
+        print('ok')
+        #future_squirrel = await commands.MemberConverter(ctx, username)
+        #if future_squirrel is None:
+        #    await ctx.channel.send('The user {} cannot be found'.format(username))
+        #    return False;
+        #future_squirrel.add_roles(squirrel_role)
+        #squirrel_channel = self.bot.get_channel(325420488107098112)
+        #if squirrel_channel is None:
+        #    await ctx.channel.send('The Squirrel Army channel cannot be found')
+        #    return False;
+        #await squirrel_channel.send('Welcome in the Squirrel Army {}, happy squirreling =^.^='.format(future_squirrel.mention))
 
 def setup(bot):
     cog = Squirrel(bot)
