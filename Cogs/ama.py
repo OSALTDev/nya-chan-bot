bb0d9aefbb33b533eccb1f12ca8fcc6522fb6bc8
Yamitatsu <cg.midev@gmail.com>
2017-07-03 11:43:56 +0200

    testing

--- Cogs/ama.py
+++ Cogs/ama.py
@@ -11,14 +11,12 @@ class Ama():
     @commands.guild_only()
     async def copyq(self, ctx):
         """Copy every question with your :upvote: reaction on it to the Question- channel"""
-        nb_message = 0
         nb_copied = 0
         destination = self.bot.get_channel(331363194780254210)
         if destination is None:
             await ctx.channel.send('There is no destination channel.')
             return False
         async for msg in ctx.channel.history(limit=200):
-            nb_message += 1
             to_copy = False
             for reaction in msg.reactions:
                 from_me = False
@@ -29,9 +27,9 @@ class Ama():
                     to_copy = True
             if to_copy:
                 nb_copied += 1
-                destination.send('From {} - {} (Processed by {}) UTC\n{}'.format(msg.author.mention, msg.created_at, ctx.author.mention, msg.content))
+                await destination.send('From {} - {} (Processed by {}) UTC\n{}'.format(msg.author.mention, msg.created_at, ctx.author.mention, msg.content))
                 await msg.delete()
-        await ctx.channel.send('{}/{} messages transferred to {}.'.format(nb_copied, nb_message, destination.name))
+        await ctx.channel.send('{} message(s) transferred to {}.'.format(nb_copied, destination.name))
                     
                       
         
@@ -39,10 +37,8 @@ class Ama():
     @commands.guild_only()
     async def cleanq(self, ctx):
         """Delete every message with your :downvote: reaction"""
-        nb_message = 0
         nb_deleted = 0
         async for msg in ctx.channel.history(limit=200):
-            nb_message += 1
             to_delete = False
             for reaction in msg.reactions:
                 from_me = False
@@ -54,7 +50,7 @@ class Ama():
             if to_delete:
                 nb_deleted += 1
                 await msg.delete()
-        await ctx.channel.send('{}/{} messages have been processed.'.format(nb_deleted, nb_message))
+        await ctx.channel.send('{} message(s) have been processed.'.format(nb_deleted))
 
     @commands.command(description='')
     @commands.guild_only()
