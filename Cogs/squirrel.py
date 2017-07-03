707e845977cbcd5e23e9c6d07cf9c5b0bf16ee89
Yamitatsu <cg.midev@gmail.com>
2017-07-04 01:02:19 +0200

    testing

--- Cogs/squirrel.py
+++ Cogs/squirrel.py
@@ -26,7 +26,7 @@ class Squirrel():
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print(exc_type, fname, exc_tb.tb_lineno)        
+            print(exc_type, fname, exc_tb.tb_lineno, e.message)        
         #if future_squirrel is None:
         #    await ctx.channel.send('The user {} cannot be found'.format(username))
         #    return False;
