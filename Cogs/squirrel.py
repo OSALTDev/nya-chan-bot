6dfb0ec45c8b4ecaaed2a2f0dddd10002e78e6ba
Yamitatsu <cg.midev@gmail.com>
2017-07-04 01:22:11 +0200

    testing

--- Cogs/squirrel.py
+++ Cogs/squirrel.py
@@ -20,22 +20,17 @@ class Squirrel():
         if squirrel_role == None:
             await ctx.channel.send('There is no Squirrel Army role on this server.')
             return False
-        try:
-            converter = commands.converter.MemberConverter()
-            future_squirrel = converter.convert(ctx, username)
-        except Exception as e:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print(exc_type, fname, exc_tb.tb_lineno, str(e))        
-        #if future_squirrel is None:
-        #    await ctx.channel.send('The user {} cannot be found'.format(username))
-        #    return False;
-        #future_squirrel.add_roles(squirrel_role)
-        #squirrel_channel = self.bot.get_channel(325420488107098112)
-        #if squirrel_channel is None:
-        #    await ctx.channel.send('The Squirrel Army channel cannot be found')
-        #    return False;
-        #await squirrel_channel.send('Welcome in the Squirrel Army {}, happy squirreling =^.^='.format(future_squirrel.mention))
+        converter = commands.converter.MemberConverter()
+        future_squirrel = await converter.convert(ctx, username)  
+        if future_squirrel is None:
+            await ctx.channel.send('The user {} cannot be found'.format(username))
+            return False;
+        future_squirrel.add_roles(squirrel_role)
+        squirrel_channel = self.bot.get_channel(325420488107098112)
+        if squirrel_channel is None:
+            await ctx.channel.send('The Squirrel Army channel cannot be found')
+            return False;
+        await squirrel_channel.send('Welcome in the Squirrel Army {}, happy squirreling =^.^='.format(future_squirrel.mention))
 
 def setup(bot):
     cog = Squirrel(bot)
