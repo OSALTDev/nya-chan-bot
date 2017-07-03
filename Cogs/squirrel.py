110227973ffe41b5b223d0a165de37d3b2d0d50c
Yamitatsu <cg.midev@gmail.com>
2017-07-04 00:50:44 +0200

    testing

---
+++ Cogs/squirrel.py
@@ -0,0 +1,35 @@
+from discord.ext import commands
+import asyncio
+import datetime
+import role_ids
+
+class Squirrel():
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(description='Add a user to the Squirrel Army.')
+    @commands.guild_only()
+    @commands.has_any_role('Nixie', 'Mods', 'Elder Squirrel')
+    async def squirreladd(self, ctx, username: str):
+        """Add a user to the Squirrel Army"""
+        squirrel_role = None
+        for x in ctx.guild.roles:
+            if x.id == 331563592560410634:  #329409494478094336
+                squirrel_role = x
+        if squirrel_role == None:
+            await ctx.channel.send('There is no Squirrel Army role on this server.')
+            return False
+        future_squirrel = commands.MemberConverter(ctx, username)
+        if future_squirrel is None:
+            await ctx.channel.send('The user {} cannot be found'.format(username))
+            return False;
+        future_squirrel.add_roles(squirrel_role)
+        squirrel_channel = self.bot.get_channel(325420488107098112)
+        if squirrel_channel is None:
+            await ctx.channel.send('The Squirrel Army channel cannot be found')
+            return False;
+        await squirrel_channel.send('Welcome in the Squirrel Army {}, happy squirreling =^.^='.format(future_squirrel.mention))
+
+def setup(bot):
+    cog = Squirrel(bot)
+    bot.add_cog(cog)
