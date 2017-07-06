0046307ada37bbb7355f35135abd4c8ab80cf647
Yamitatsu <cg.midev@gmail.com>
2017-07-06 21:26:40 +0200

    test

--- nyalib/config.py
+++ nyalib/config.py
@@ -4,19 +4,21 @@ import yaml
 
 from nyalib.bot_config import BotConfig
 
+
 class Singleton(object):
     """
     This class is used to insure that you  only a single instance of the object is ever created.
     Since we're using a relative path this should be invoked from NyaChan.py first.
     """
     _instance = None
+
     def __new__(class_, *args, **kwargs):
         if not isinstance(class_._instance, class_):
             class_._instance = object.__new__(class_, *args, **kwargs)
             class_.__contruct__(class_)
         return class_._instance
 
-    def __contruct__(class_ ):
+    def __contruct__(class_):
         try:
             config_stream = open('config/settings.yaml', 'r')
             class_._instance.config = yaml.load(config_stream)
@@ -24,7 +26,7 @@ class Singleton(object):
         except Exception as err:
             pass
 
-        if(class_._instance.config is not None):
+        if class_._instance.config is not None:
             class_.bot = BotConfig(**class_._instance.config['bot'])
 
 
@@ -36,7 +38,8 @@ class AppConfig(Singleton):
     def db_connection(self):
         db_config = self.config['database']
         return pymysql.connect(
-            host=db_config['host'], user=db_config['user'], password=db_config['password'], db=db_config['database'], charset='utf8')
+            host=db_config['host'], user=db_config['user'], password=db_config['password'], db=db_config['database'],
+            charset='utf8')
 
     @property
     def git_branch(self):
