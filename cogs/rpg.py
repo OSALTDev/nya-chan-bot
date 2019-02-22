ba5e929b9697728548eb1aa952f304be706bab59
Aaron Lee Marais <aaronleem@gmail.com>
2019-02-22 05:24:36 +0200

    Some formatting

--- cogs/rpg.py
+++ cogs/rpg.py
@@ -14,15 +14,16 @@ class RPG(BaseCog):
         bot_channel = self.bot.get_channel(332644650462478336)
         if bot_channel is None:
             await ctx.channel.send('The dedicated bot commands channel cannot be found')
-            return False
+            return
+
         try:
             rolls, limit = map(int, dice.split('d'))
         except Exception:
             await bot_channel.send('```{}, format has to be NdN !```'.format(ctx.author.mention))
-            return False
-        result = [random.randint(1, limit) for r in range(rolls)]
-        msg = "```Results for {} : {} (Total : {})```".format(dice, ', '.join(str(x) for x in result), sum(result))
-        await bot_channel.send(msg)
+        else:
+            result = [random.randint(1, limit) for _ in range(rolls)]
+            msg = "```Results for {} : {} (Total : {})```".format(dice, ', '.join(str(x) for x in result), sum(result))
+            await bot_channel.send(msg)
 
 
 def setup(bot):
