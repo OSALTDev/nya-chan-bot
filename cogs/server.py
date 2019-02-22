86e780fa0301bb1b96e03c5b4075e4ba55d346f7
Aaron Lee Marais <aaronleem@gmail.com>
2019-02-22 05:29:16 +0200

    Clean this heck up

--- cogs/server.py
+++ cogs/server.py
@@ -19,32 +19,20 @@ class Server(BaseCog):
         disk_usage = subprocess.getoutput("df -h")
         listening_stats = subprocess.getoutput("netstat -l | grep -P '(\*:|\[::]:)'")
         active_connections_v4 = subprocess.getoutput(
-            "netstat -Wn --numeric-ports -a -4 | grep -v LISTEN | tail -n +3 | awk '{print $4}' | cut -d':' -f1 | uniq -c | sort -r ")
-        msg = """```
-OS: {} {} ({})
+            "netstat -Wn --numeric-ports -a -4 | grep -v LISTEN | tail -n +3 | "
+            "awk '{print $4}' | cut -d':' -f1 | uniq -c | sort -r "
+        )
 
-System Uptime: {} days
-
-Load Averages: {} {} {}
-
-Ram usage:
-
-{}
-
-Disk Usage:
-
-{}
-
-Servers Listening:
-
-{}
-
-Active Connections (v4):
-
-{}
-        ```""".format(os_info[0], os_info[1], os_info[2], str(uptime_days), loadavg[0], loadavg[1], loadavg[2],
-                      ram_usage, disk_usage, listening_stats, active_connections_v4)
-        await ctx.channel.send(msg)
+        msg = [
+            "OS: {} {} ({})".format(os_info[0], os_info[1], os_info[2]),
+            "System Uptime: {} days".format(str(uptime_days)),
+            "Load Averages: {} {} {}".format(loadavg[0], loadavg[1], loadavg[2]),
+            "RAM usage:\n\n{}".format(ram_usage),
+            "Disk Usage:\n\n{}".format(disk_usage),
+            "Servers Listening:\n\n{}".format(listening_stats),
+            "Active Connections (v4):\n\n{}".format(active_connections_v4)
+        ]
+        await ctx.channel.send("\n\n".join(msg))
 
 
 def setup(bot):
