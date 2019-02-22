91c9800342b661dfffdfea6fd1897534eddca042
Aaron Lee Marais <aaronleem@gmail.com>
2019-02-22 04:41:45 +0200

    Use discord.utils.get Give more information in error message

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 from discord.ext.commands import group
 from cogs.base_cog import BaseCog
@@ -15,39 +16,32 @@ class Moderation(BaseCog):
 
     @mod.command(description='Promotes an user to be a Mod')
     @commands.has_any_role('Nixie', 'Supervisors')
-    async def set(self, ctx, *username):
+    async def set(self, ctx, *, username: str):
         """Promotes an user to be a Mod"""
-        user_name = " ".join(username)
         converter = commands.converter.MemberConverter()
-        user = await converter.convert(ctx, user_name)
+        user = await converter.convert(ctx, username)
         if user is None:
-            await self.bot_reply(ctx, 'The user **{}** cannot be found, {}'.format(user_name, ctx.author.mention))
-        mod_role = None
-        moderator_role = None
-        mod_trainee_role = None
-        for role in ctx.guild.roles:
-            if role.name == 'Mods':
-                mod_role = role
-            elif role.name == 'Mod Trainees':
-                mod_trainee_role = role
-            elif role.name == 'Moderators':
-                moderator_role = role
+            await self.bot_reply(ctx, 'The user **{}** cannot be found, {}'.format(username, ctx.author.mention))
+            return
+
+        mod_role = discord.utils.get(ctx.guild.roles, name="Mods")
+        moderator_role = discord.utils.get(ctx.guild.roles, name="Moderators")
+        mod_trainee_role = discord.utils.get(ctx.guild.roles, name="Mod Trainees")
+
+        unfound = []
         if mod_role is None:
-            await self.bot_reply(ctx, 'The Mods role cannot be found, {}'.format(ctx.author.mention))
-            return False
+            unfound.append("Mods")
         if moderator_role is None:
-            await self.bot_reply(ctx, 'The Moderators role cannot be found, {}'.format(ctx.author.mention))
-            return False
+            unfound.append("Moderators")
         if mod_trainee_role is None:
-            await self.bot_reply(ctx, 'The Mod Trainees role cannot be found, {}'.format(ctx.author.mention))
+            unfound.append("Mod Trainees")
+
+        if unfound:
+            await self.bot_reply(ctx, 'These roles cannot be found, {}: '.format(ctx.author.mention))
             return False
-        has_mod_role = False
-        has_trainee_role = False
-        for user_role in user.roles:
-            if user_role.name == 'Mods':
-                has_mod_role = True
-            elif user_role.name == 'Mod Trainees':
-                has_trainee_role = True
+
+        has_mod_role = discord.utils.get(user.roles, name="Mods") is not None
+        has_trainee_role = discord.utils.get(user.roles, name="Mod Trainees") is not None
         if has_mod_role is True:
             await self.bot_reply(ctx, '{} is a Mod already, {}'.format(user.name, ctx.author.mention))
             return False
@@ -61,74 +55,67 @@ class Moderation(BaseCog):
 
     @mod.command(description='Removes an user Mod status')
     @commands.has_any_role('Nixie', 'Supervisors')
-    async def remove(self, ctx, *username):
+    async def remove(self, ctx, *, username):
         """Removes an user Mod status"""
-        user_name = " ".join(username)
         converter = commands.converter.MemberConverter()
-        user = await converter.convert(ctx, user_name)
+        user = await converter.convert(ctx, username)
         if user is None:
-            await self.bot_reply(ctx, 'The user **{}** cannot be found, {}'.format(user_name, ctx.author.mention))
-        mod_role = None
-        moderator_role = None
-        for role in ctx.guild.roles:
-            if role.name == 'Mods':
-                mod_role = role
-            elif role.name == 'Moderators':
-                moderator_role = role
+            await self.bot_reply(ctx, 'The user **{}** cannot be found, {}'.format(username, ctx.author.mention))
+            return
+
+        mod_role = discord.utils.get(ctx.guild.roles, name="Mods")
+        moderator_role = discord.utils.get(ctx.guild.roles, name="Moderators")
+
+        unfound = []
         if mod_role is None:
-            await self.bot_reply(ctx, 'The Mods role cannot be found, {}'.format(ctx.author.mention))
-            return False
+            unfound.append("Mods")
         if moderator_role is None:
-            await self.bot_reply(ctx, 'The Moderators role cannot be found, {}'.format(ctx.author.mention))
-            return False
-        has_mod_role = False
-        for user_role in user.roles:
-            if user_role.name == 'Mods':
-                has_mod_role = True
-        if has_mod_role is not True:
+            unfound.append("Moderators")
+
+        if unfound:
+            await self.bot_reply(ctx, 'These roles cannot be found, {}: {}'.format(ctx.author.mention, ", ".join(unfound)))
+            return
+
+        has_mod_role = discord.utils.get(user.roles, name="Mods") is not None
+        if has_mod_role is False:
             await self.bot_reply(ctx, '{} is a not a Mod, {}'.format(user.name, ctx.author.mention))
-            return False
+            return
+
         await user.remove_roles(mod_role, moderator_role, reason="User demoted from Mods")
         await self.bot_reply(ctx, '{} is not a Mod anymore, {}'.format(user.name, ctx.author.mention))
 
     @mod.command(description='Promotes an user to be a Mod Trainee')
     @commands.has_any_role('Nixie', 'Supervisors')
-    async def set_trainee(self, ctx, *username):
+    async def set_trainee(self, ctx, *, username):
         """Promotes an user to be a Mod Trainee"""
-        user_name = " ".join(username)
         converter = commands.converter.MemberConverter()
-        user = await converter.convert(ctx, user_name)
+        user = await converter.convert(ctx, username)
         if user is None:
-            await self.bot_reply(ctx, 'The user **{}** cannot be found, {}'.format(user_name, ctx.author.mention))
-        mod_role = None
-        moderator_role = None
-        mod_trainee_role = None
-        for role in ctx.guild.roles:
-            if role.name == 'Mods':
-                mod_role = role
-            elif role.name == 'Mod Trainees':
-                mod_trainee_role = role
-            elif role.name == 'Moderators':
-                moderator_role = role
+            await self.bot_reply(ctx, 'The user **{}** cannot be found, {}'.format(username, ctx.author.mention))
+            return
+
+        mod_role = discord.utils.get(ctx.guild.roles, name="Mods")
+        moderator_role = discord.utils.get(ctx.guild.roles, name="Moderators")
+        mod_trainee_role = discord.utils.get(ctx.guild.roles, name="Mod Trainees")
+
+        unfound = []
         if mod_role is None:
-            await self.bot_reply(ctx, 'The Mod role cannot be found, {}'.format(ctx.author.mention))
-            return False
+            unfound.append("Mods")
         if moderator_role is None:
-            await self.bot_reply(ctx, 'The Moderators role cannot be found, {}'.format(ctx.author.mention))
-            return False
+            unfound.append("Moderators")
         if mod_trainee_role is None:
-            await self.bot_reply(ctx, 'The Mod Trainees role cannot be found, {}'.format(ctx.author.mention))
-            return False
-        has_mod_role = False
-        has_trainee_role = False
-        for user_role in user.roles:
-            if user_role.name == 'Mods':
-                has_mod_role = True
-            elif user_role.name == 'Mod Trainees':
-                has_trainee_role = True
+            unfound.append("Mod Trainees")
+
+        if unfound:
+            await self.bot_reply(ctx, 'These roles cannot be found, {}: '.format(ctx.author.mention))
+            return
+
+        has_trainee_role = discord.utils.get(user.roles, name="Mod Trainees") is not None
         if has_trainee_role is True:
             await self.bot_reply(ctx, '{} is a Mod Trainee already, {}'.format(user.name, ctx.author.mention))
-            return False
+            return
+
+        has_mod_role = discord.utils.get(user.roles, name="Mods") is not None
         if has_mod_role is True:
             await user.add_roles(mod_trainee_role, reason="Promoted to Mod Trainees")
             user.roles.append(mod_trainee_role)
@@ -146,32 +133,28 @@ class Moderation(BaseCog):
         user = await converter.convert(ctx, user_name)
         if user is None:
             await self.bot_reply(ctx, 'The user **{}** cannot be found, {}'.format(user_name, ctx.author.mention))
-        mod_role = None
-        moderator_role = None
-        mod_trainee_role = None
-        for role in ctx.guild.roles:
-            if role.name == 'Mods':
-                mod_role = role
-            elif role.name == 'Mod Trainees':
-                mod_trainee_role = role
-            elif role.name == 'Moderators':
-                moderator_role = role
+
+        mod_role = discord.utils.get(ctx.guild.roles, name="Mods")
+        moderator_role = discord.utils.get(ctx.guild.roles, name="Moderators")
+        mod_trainee_role = discord.utils.get(ctx.guild.roles, name="Mod Trainees")
+
+        unfound = []
         if mod_role is None:
-            await self.bot_reply(ctx, 'The Mods role cannot be found, {}'.format(ctx.author.mention))
-            return False
+            unfound.append("Mods")
         if moderator_role is None:
-            await self.bot_reply(ctx, 'The Moderators role cannot be found, {}'.format(ctx.author.mention))
-            return False
+            unfound.append("Moderators")
         if mod_trainee_role is None:
-            await self.bot_reply(ctx, 'The Mod Trainees role cannot be found, {}'.format(ctx.author.mention))
-            return False
-        has_trainee_role = False
-        for user_role in user.roles:
-            if user_role.name == 'Mod Trainees':
-                has_trainee_role = True
-        if has_trainee_role is not True:
+            unfound.append("Mod Trainees")
+
+        if unfound:
+            await self.bot_reply(ctx, 'These roles cannot be found, {}: '.format(ctx.author.mention))
+            return
+
+        has_trainee_role = discord.utils.get(user.roles, name="Mod Trainees") is not None
+        if has_trainee_role is False:
             await self.bot_reply(ctx, '{} is a not a Mod Trainee, {}'.format(user.name, ctx.author.mention))
-            return False
+            return
+
         await user.remove_roles(mod_trainee_role, moderator_role, reason="User demoted from Mod Trainees")
         await self.bot_reply(ctx, '{} is not a Mod Trainee anymore, {}'.format(user.name, ctx.author.mention))
 
