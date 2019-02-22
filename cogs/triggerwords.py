f40947c4b8b1268070d8c66f23e4ce73a948319e
Aaron Lee Marais <aaronleem@gmail.com>
2019-02-22 07:33:34 +0200

    Use re.compile() Dont use dict to store what should be non-dicted items

--- cogs/triggerwords.py
+++ cogs/triggerwords.py
@@ -2,14 +2,11 @@ import re
 
 from discord import Message
 from discord.ext.commands import Bot
+from types import SimpleNamespace
 
 
 # Add regex here, along with the response the bot should give if it finds a match.
 # We're using {bot} and {user} to refer
-RESPONSES = {
-    r"(:?i'?m|i am) (going|gonna go) to (:?bed|sleep)": "Good night {user}!",
-    r"(:?hi|hello|hey there|sup) {bot}!?": "Hey {user}!",
-}
 
 
 class TriggerWords:
@@ -19,22 +16,24 @@ class TriggerWords:
 
     def __init__(self, bot: Bot):
         self.bot = bot
+        self.triggers = None
 
     async def on_ready(self):
         """
-        Replaces {bot} with the bot mention after
+        Generates the regex and responses once
         the bot has booted. This information is not
         available until runtime.
         """
-
-        keys = list(RESPONSES.keys())
-
-        for key in keys:
-            changed_key = key.replace("{bot}", self.bot.user.mention)
-
-            if key != changed_key:
-                RESPONSES[changed_key] = RESPONSES[key]
-                del RESPONSES[key]
+        self.triggers = [
+            SimpleNamespace(
+                regex=re.compile("(:?i'?m|i am) (going|gonna go) to (:?bed|sleep)"),
+                response="Good night {}!"
+            ),
+            SimpleNamespace(
+                regex=re.compile(f"(:?hi|hello|hey there|sup) {self.bot.user.mention}!?"),
+                response="Hey {}!"
+            )
+        ]
 
     async def on_message(self, message: Message):
         """
@@ -43,13 +42,14 @@ class TriggerWords:
         If the content of the message matches one of our triggers, we'll respond
         with the corresponding message.
         """
-
-        content = message.content
+        if not self.triggers:
+            return
 
         response = None
-        for trigger in RESPONSES.keys():
-            if re.search(trigger, content):
-                response = RESPONSES.get(trigger)
+        for trigger in self.triggers.keys():
+            if trigger.regex.search(message.content):
+                response = trigger.response
+                break
 
         if response:
             await message.channel.send(response.format(user=message.author.mention))
