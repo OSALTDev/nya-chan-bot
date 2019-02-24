e4e63e08cd9de999e0b293e738feaf9195f65852
Aaron Lee Marais <aaronleem@gmail.com>
2019-02-24 12:31:31 +0200

    Change "tch" to "channel" Remove __init__ for Channels cog Send status message for any of the 3 results

--- cogs/channels.py
+++ cogs/channels.py
@@ -5,16 +5,13 @@ from cogs.base_cog import BaseCog
 
 
 class Channels(BaseCog):
-    def __init__(self, bot):
-        super().__init__(bot)
-
     @group()
-    async def tch(self, ctx):
+    async def channel(self, ctx):
         """Text Channel edition commands."""
         if ctx.invoked_subcommand is None:
             await ctx.reply('Invalid Text Channel Edition command passed')
 
-    @tch.command(description='Create a new text channel with Supervisor permission.')
+    @channel.command(description='Create a new text channel with Supervisor permission.')
     @commands.has_any_role('Nixie', 'Supervisors')
     @commands.guild_only()
     async def create(self, ctx, *, channel_name: str):
@@ -22,14 +19,18 @@ class Channels(BaseCog):
         if discord.utils.get(ctx.guild.text_channels, name=channel_name) is not None:
             await ctx.reply('Channel **{}** already exists'.format(channel_name))
             return
+
         supervisor_role = discord.utils.get(ctx.guild.roles, name="Supervisors")
         if supervisor_role is None:
+            await ctx.reply("Supervisor role is required to create")
             return
+
         perms = {
             ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
             supervisor_role: discord.PermissionOverwrite(read_messages=True, manage_roles=True)
         }
         await ctx.guild.create_text_channel(channel_name, overwrites=perms)
+        await ctx.reply("Created text channel {}".format(channel_name))
 
 
 def setup(bot):
