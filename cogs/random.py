a42f520ac8578b846d20b35657be58befe5a647b
Aaron Lee Marais <aaronleem@gmail.com>
2019-02-24 17:56:56 +0200

    Remove __init__ from Random

--- cogs/random.py
+++ cogs/random.py
@@ -1,17 +1,13 @@
-import random
 from discord.ext import commands
 from cogs.base_cog import BaseCog
 
 
 class Random(BaseCog):
-    def __init__(self, bot):
-        super().__init__(bot)
-
     @commands.command(description='Explains Time Blindness')
     async def nixietime(self, ctx):
-        msg = "F(NixieTime) is defined as a function of F that represents the current time plus a non-deterministic random " \
-              "value denoting a temporal period.  The exact value of the delta is unknown and changes constantly based on " \
-              "fluctuation in the space time continuum and doctor who paradoxes"
+        msg = "F(NixieTime) is defined as a function of F that represents the current time plus a non-deterministic " \
+              "random value denoting a temporal period.  The exact value of the delta is unknown and changes " \
+              "constantly based on fluctuation in the space time continuum and doctor who paradoxes"
         await ctx.channel.send(msg)
 
 
