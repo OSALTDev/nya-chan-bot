36ede7447061330f27b94df062385950ab10d9b5
Aaron Lee Marais <aaronleem@gmail.com>
2019-03-05 23:03:29 +0200

    Change these to static methods

--- database/__init__.py
+++ database/__init__.py
@@ -7,10 +7,10 @@ class Methods:
     def select(table_name, distinct=False, limit=None, order=None, where=None, items=None):
         return query.Select(table_name, distinct=distinct, limit=limit, order=order, where=where, items=items)
 
-    @classmethod
-    def insert(cls, table_name, **kwargs):
+    @staticmethod
+    def insert(table_name, **kwargs):
         return query.Insert(table_name=table_name, items=kwargs)
 
-    @classmethod
-    def update(cls, table_name, items=None, where=None):
+    @staticmethod
+    def update(table_name, items=None, where=None):
         return query.Update(table_name=table_name, items=items, where=where)
