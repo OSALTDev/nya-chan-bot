bb526e145b42bc8b25f7fa21ab7f9ca9214ef78e
Aaron Lee Marais <aaronleem@gmail.com>
2019-06-19 14:22:59 +0200

    Add BotConfig to __all__ and add database defaults

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,7 +10,7 @@ handler = logging.FileHandler(filename='debug.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-__all__ = ("BotBase",)
+__all__ = ("BotBase", "BotConfig")
 
 
 class BotBase(commands.Bot):
