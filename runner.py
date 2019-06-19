17b42899101d1379d932a5eaef2e640bc325a6e0
Aaron Lee Marais <aaronleem@gmail.com>
2019-06-19 15:22:06 +0200

    Abstract cog loading, and log errors

--- runner.py
+++ runner.py
@@ -1,12 +1,44 @@
 import bot
+import os
+import traceback
+import logging
 from discord.ext import commands
 
 BotInstance = bot.BotBase()
 
-for cog in bot.BotConfig.cogs:
+
+def _load_and_print_if_error(name):
     try:
-        BotInstance.load_extension(cog)
+        BotInstance.load_extension(name)
+    except commands.ExtensionAlreadyLoaded:
+        pass
     except commands.ExtensionError as e:
-        print(f"Failed to load cog: {cog['file']} due to {e}")
+        print(f"Failed to load cog: {name}")
+
+        if bot.BotConfig.debug:
+            logger = logging.getLogger('nya')
+            logger.setLevel(logging.DEBUG)
+            handler = logging.FileHandler(filename='nya-debug.log', encoding='utf-8', mode='w')
+            handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+            logger.addHandler(handler)
+
+            logger.error(traceback.format_exc())
+            logger.error(e)
+
+
+cogs = bot.BotConfig.cogs
+if not cogs:
+    cogs = [
+        f.replace('.py', '') for f in os.listdir("cogs")
+        if os.path.isfile(os.path.join("cogs", f))
+        and f.split(".")[-1] == "py"
+    ]
+
+for cog in cogs:
+    _load_and_print_if_error(f"cogs.{cog}")
+
+# Required cogs
+_load_and_print_if_error("cogs.core")
+_load_and_print_if_error("jishaku")
 
 BotInstance.run(bot.BotConfig.token)
