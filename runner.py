9bd210a499eb3011d5afb85c89f30e5e405511ed
Aaron Lee Marais <aaronleem@gmail.com>
2019-06-19 14:49:55 +0200

    Cog loader

--- runner.py
+++ runner.py
@@ -1,4 +1,12 @@
 import bot
+from discord.ext import commands
 
 BotInstance = bot.BotBase()
+
+for cog in bot.BotConfig.cogs:
+    try:
+        BotInstance.load_extension(cog)
+    except commands.ExtensionError as e:
+        print(f"Failed to load cog: {cog['file']} due to {e}")
+
 BotInstance.run(bot.BotConfig.token)
