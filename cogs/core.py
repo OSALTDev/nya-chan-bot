1fa8df08be5ad2da476c380c833034a8add6693e
Aaron Lee Marais <aaronleem@gmail.com>
2019-06-20 12:31:12 +0200

    Comment core cog, make helper method a static method

--- cogs/core.py
+++ cogs/core.py
@@ -1,10 +1,15 @@
 """
 Cog for base events (such as logging, etc)
+
+Events are only logged if debug is on, or if the
+specific event is enabled in the config.yaml
 """
 
+# Comprehensive error and logging
 import logging
 import traceback
 
+# Import cog base and re-use commands; import our config too
 from bot.cog_base import Base, commands
 from bot.config import Config, Logging
 
@@ -13,32 +18,41 @@ class setup(Base, name="Core"):
     def __init__(self, bot):
         super().__init__(bot)
 
+        # Create event loggers
         self.command_log = self._create_logger_for("commands")
 
-    def _create_logger_for(self, logger_name):
+    # Helper function to create logger instances
+    @staticmethod
+    def _create_logger_for(logger_name):
         logger = logging.getLogger(f'nya.{logger_name}')
 
         logger.setLevel(logging.INFO)
         handler = logging.FileHandler(filename=f'logs/nya.{logger_name}.log', encoding='utf-8', mode='w')
 
+        # time:LEVEL:logname: message
         handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
         logger.addHandler(handler)
 
         return logger
 
+    # Log commands being executed
     @commands.Cog.listener()
     async def on_command(self, ctx: commands.Context):
-        if Config.debug and Logging.Command.execute:
+        if Config.debug or Logging.Command.execute:
             self.command_log.info(f"User {ctx.author} executing command {ctx.invoked_with}")
 
+    # Log command errors
     @commands.Cog.listener()
     async def on_command_error(self, _, error):
-        if Config.debug and Logging.Command.error:
-            self.command_log.debug(f"Above command gave error")
+        if Config.debug or Logging.Command.error:
+            # Get exception info, in a list
             formatted_exception = traceback.format_exception(type(error), error, None)
-            self.command_log.error('\n' + "".join(formatted_exception).strip())
+            self.command_log.error(
+                f"Above command gave error\n" + "".join(formatted_exception).strip()
+            )
 
+    # Log command completions
     @commands.Cog.listener()
     async def on_command_completion(self, ctx):
-        if Config.debug and Logging.Command.complete:
+        if Config.debug or Logging.Command.complete:
             self.command_log.info(f"Command complete")
