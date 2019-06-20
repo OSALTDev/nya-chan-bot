be5f163835c7ec47b2c5b6474ab11a8f4526523a
Aaron Lee Marais <aaronleem@gmail.com>
2019-06-20 11:12:06 +0200

    Add cog to bot, and import BotBase for PyCharm context

--- bot/cog_base.py
+++ bot/cog_base.py
@@ -1,6 +1,8 @@
+from bot import BotBase
 from discord.ext import commands
 
 
 class Base(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: BotBase):
         self.bot = bot
+        self.bot.add_cog(self)
--- cogs/core.py
+++ cogs/core.py
@@ -1,6 +1,50 @@
-from bot.cog_base import Base
+"""
+Cog for base events (such as logging, etc)
+"""
+
+import logging
+import traceback
+
+from bot.cog_base import Base, commands
+from bot.config import Bot as BotConfig
 
 
 class setup(Base, name="Core"):
     def __init__(self, bot):
         super().__init__(bot)
+
+        self.command_log = self._create_logger_for("commands")
+
+    def _create_logger_for(self, logger_name):
+        logger = logging.getLogger(f'nya.{logger_name}')
+
+        logger.setLevel(logging.INFO)
+        handler = logging.FileHandler(filename=f'logs/nya.{logger_name}.log', encoding='utf-8', mode='w')
+
+        handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+        logger.addHandler(handler)
+
+        return logger
+
+    @commands.Cog.listener()
+    async def on_command(self, ctx: commands.Context):
+        if BotConfig.debug:
+            self.command_log.info(f"User {ctx.author} executing command {ctx.invoked_with}")
+
+    @commands.Cog.listener()
+    async def on_command_error(self, _, error):
+        if BotConfig.debug:
+            self.command_log.debug(f"Above command gave error")
+            self.command_log.error(traceback.format_exc())
+            self.command_log.error(error)
+
+    @commands.Cog.listener()
+    async def on_command_completion(self, ctx):
+        if BotConfig.debug:
+            self.command_log.debug(f"Command complete")
+
+    @commands.command()
+    async def test(self, ctx, hi: int = 5):
+        print(hi)
+        print("hi")
+
