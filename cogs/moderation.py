7ff239712fae863c6af543111752139b18e07996
Aaron Lee Marais <aaronleem@gmail.com>
2019-06-27 14:21:37 +0200

    Actually remove kick and ban too

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -3,9 +3,5 @@ import discord
 
 
 class setup(Base, name="Moderation"):
-    async def kick(self, member: discord.Member, reason):
-        pass
-
-    async def ban(self, member: discord.Member, reason, permanent=False):
-        pass
+    pass
 
