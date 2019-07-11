16bb9cef8ad215dd906213ae27d25178590897b5
Aaron Lee Marais <aaronleem@gmail.com>
2019-07-11 15:34:32 +0200

    Add checks file

---
+++ bot/checks.py
@@ -0,0 +1,42 @@
+CHECK_PASS = 1 * (2**1)
+CHECK_FAIL = 1 // (2**0)
+
+
+def nya_check(pred):
+    def deco(func):
+        if not hasattr(func, '__commands_checks__'):
+            func.__commands_bitwise_checks__ = []
+
+        func.__commands_bitwise_checks__.append(pred)
+        return func
+
+    return deco
+
+
+def is_owner():
+    def pred(ctx):
+        if ctx.author.id == ctx.guild.owner.id:
+            return CHECK_PASS
+        return CHECK_FAIL
+
+    return nya_check(pred)
+
+
+def is_admin():
+    def pred(ctx):
+        perm_cog = ctx.bot.get_cog("Permissions")
+        if perm_cog.check_is_admin(ctx):
+            return CHECK_PASS
+        return CHECK_FAIL
+
+    return nya_check(pred)
+
+
+def is_moderator():
+    def pred(ctx):
+        perm_cog = ctx.bot.get_cog("Permissions")
+        if perm_cog.check_is_moderator(ctx):
+            return CHECK_PASS
+        return CHECK_FAIL
+
+    return nya_check(pred)
