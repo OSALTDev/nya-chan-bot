bb1361aa0eae2e7da47948df519578558d9baa43
Aaron Lee Marais <aaronleem@gmail.com>
2019-07-11 12:13:17 +0200

    Custom command class, adding bitwise checks for permissions module

---
+++ bot/command.py
@@ -0,0 +1,18 @@
+"""
+    This file holds our custom command class
+    It adds a bitwise check option to the Discord command class
+"""
+
+from discord.ext import commands
+
+
+class NyaCommand(commands.Command):
+    def __init__(self, func, **kwargs):
+        super().__init__(func, **kwargs)
+
+        try:
+            bitwise_checks = func.__commands_bitwise_checks__
+            bitwise_checks.reverse()
+            self.bitwise_checks = bitwise_checks
+        except AttributeError:
+            pass
