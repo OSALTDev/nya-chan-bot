faf0801921c9ed98d2a682977890fd23f9bd27f2
Aaron Lee Marais <aaronleem@gmail.com>
2019-07-15 00:25:35 +0200

    Fix SH start script

--- start.sh
+++ start.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-VERSION=$(python -V 2>&1 | cut -d\  -f 2) # python 2 prints version to stderr
-VERSION=(${VERSION//./ })
-if [[ ${VERSION[0]} -lt 3 ]] || [[ ${VERSION[0]} -eq 3 && ${VERSION[1] -lt 7} ]] ; then
+VERSION=$(python -V 2>&1 | cut -d\  -f 2 | cut -d. -f 1,2) # python 2 prints version to stderr
+if [ $VERSION != '3.7' ] ; then
     echo "Python 3.7+ needed!"
-    return 1
+else
+    export TOKEN="MyToken"
+    python ./runner.py
 fi
-
-TOKEN="MyToken"
-python ./runner.py
-
