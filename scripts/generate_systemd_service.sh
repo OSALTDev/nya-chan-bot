affe8f13131769f9e3c603554a6b633c616dbba7
Aaron Lee Marais <aaronleem@gmail.com>
2019-07-15 16:28:06 +0200

    Directly execute the runner file

--- scripts/generate_systemd_service.sh
+++ scripts/generate_systemd_service.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
-#!/bin/bash
 
-C_DIR=`dirname "$(readlink -f $0)"`
+VERSION=$(python -V 2>&1 | cut -d\  -f 2 | cut -d. -f 1,2) # python 2 prints version to stderr
+if [[ ${VERSION} != '3.7' ]] ; then
+    echo "Python 3.7+ needed!"
+    exit 1
+fi
+
+C_DIR=`dirname "$(readlink -f $0)"`/..
+PYTHON_EXECUTABLE=`python -c "import sys; print(sys.executable)"`
 cat > nyachan.service <<- EOM
 [Unit]
 Description=Nya-Chan Discord bot service
 
 [Service]
 Type=simple
-ExecStart=/bin/bash ${C_DIR}/start.sh
-ExecStop=/bin/bash ${C_DIR}/stop.sh
+ExecStart=${PYTHON_EXECUTABLE} ${C_DIR}/runner.py
+ExecStop=/bin/bash ${C_DIR}/script/stop.sh
 User=
 Group=
 
 [Install]
 WantedBy=multi-user.target
 EOM
+
