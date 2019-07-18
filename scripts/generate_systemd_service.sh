07c6851bdbfaca7383ab8626251b6fff07ba3d15
Aaron Lee Marais <aaronleem@gmail.com>
2019-07-18 11:41:04 +0200

    Proper check if python is <370 in bash, add comments to bash files

--- scripts/generate_systemd_service.sh
+++ scripts/generate_systemd_service.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 
+# Check if argument is passed, and set PYTHON_LAUNCHER var to the argument passed
+# If not passed, just use "python"
 if [[ -z "$1" ]] ; then
     PYTHON_LAUNCHER=python
 else
     PYTHON_LAUNCHER=$1
 fi
 
-VERSION=$(${PYTHON_LAUNCHER} -V 2>&1 | cut -d\  -f 2 | cut -d. -f 1,2) # python 2 prints version to stderr
+# Get python launcher (cut by spaces, get second result, then cut by "b" (beta), and get first result)
+VERSION=$(${PYTHON_LAUNCHER} -V 2>&1 | cut -d\  -f 2 | cut -db -f 1) # python 2 prints version to stderr
 
-if [[ ${VERSION} != '3.7' ]] ; then
+# Compare version with periods replaced by nothing, to 370 (python 3.7.0 +)
+# If lower than, display message for error, and
+if [[ "${VERSION//./}" -lt '370' ]] ; then
     echo "Python 3.7+ needed!"
     exit 1
 fi
 
+# Change into parent dir of script
 C_DIR=`pwd`
 cd `dirname "$(readlink -f $0)"`/..
-cat > ${C_DIR}/nyachan.service <<- EOM
+
+# Cat our script into nyachan.service
+cat <<- EOM > ${C_DIR}/nyachan.service
 [Unit]
 Description=Nya-Chan Discord bot service
 
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+# Check if argument is passed, and set PYTHON_LAUNCHER var to the argument passed
+# If not passed, just use "python"
+if [[ -z "$1" ]] ; then
+    PYTHON_LAUNCHER=python
+else
+    PYTHON_LAUNCHER=$1
+fi
+
+# Get python launcher (cut by spaces, get second result, then cut by "b" (beta), and get first result)
+VERSION=$(${PYTHON_LAUNCHER} -V 2>&1 | cut -d\  -f 2 | cut -db -f 1) # python 2 prints version to stderr
+
+# Compare version with periods replaced by nothing, to 370 (python 3.7.0 +)
+# If lower than, display message for error, and
+if [[ "${VERSION//./}" -lt '370' ]] ; then
+    echo "Python 3.7+ needed!"
+    exit 1
+fi
+
 C_DIR=`dirname "$(readlink -f $0)"`
 
 read -r pid < ${C_DIR}/../.pid
