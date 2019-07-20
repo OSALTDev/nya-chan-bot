ba375c1b8825163c445cc63af16c7b21a6af3110
Aaron Lee Marais <aaronleem@gmail.com>
2019-07-20 14:46:03 +0200

    Don't send help info for commands that are disabled

--- bot/help.py
+++ bot/help.py
@@ -16,3 +16,21 @@ class NyaHelp(DefaultHelpCommand):
                 for line in command.help.splitlines():
                     self.paginator.add_line(line)
                 self.paginator.add_line()
+
+    def disabled_command(self, command):
+        permissions_cog = self.context.bot.get_cog("Permissions")
+        guild_config = permissions_cog.db.find(guild_id=str(self.context.guild.id))
+        return "disabled_commands" in guild_config.getStore() and \
+               command.qualified_name in guild_config["disabled_commands"]
+
+    async def send_command_help(self, command):
+        if self.disabled_command(command):
+            return False
+
+        await super().send_command_help(command)
+
+    async def send_group_help(self, group):
+        if self.disabled_command(group):
+            return False
+
+        await super().send_group_help(group)
